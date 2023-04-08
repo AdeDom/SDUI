707c65acef0c6bda6b06d9428357b06771c24223
pathiphon <pathiphondev@gmail.com>
2023-04-08 15:34:52 +0700

    Setup main activity.

---
+++ app/src/main/java/com/adedom/sdui/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.adedom.sdui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.adedom.ui_component.Greeting
+import com.adedom.ui_component.ui.theme.SDUITheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SDUITheme {
+                Greeting("Android")
+            }
+        }
+    }
+}
---
+++ core/ui-component/src/main/java/com/adedom/ui_component/Greeting.kt
@@ -0,0 +1,19 @@
+package com.adedom.ui_component
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.adedom.ui_component.ui.theme.SDUITheme
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    SDUITheme {
+        Greeting("Android")
+    }
+}
