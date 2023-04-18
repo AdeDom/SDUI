cf6c96ded9bf9381fa25fe18b31d771efd725d95
pathiphon <pathiphondev@gmail.com>
2023-04-18 10:05:42 +0700

    Setup adaptive card module.

---
+++ feature/adaptivecards/src/main/java/com/adedom/adaptivecards/presentation/component/AdaptiveCardScreen.kt
@@ -0,0 +1,9 @@
+package com.adedom.adaptivecards.presentation.component
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AdaptiveCardScreen() {
+    Text(text = "Hello, Adaptive card.")
+}
