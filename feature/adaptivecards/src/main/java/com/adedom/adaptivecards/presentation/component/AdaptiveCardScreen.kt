e12fbe822e2fe2429e60d7e3b2ca5aa137d3ac51
pathiphon <pathiphondev@gmail.com>
2023-04-20 17:01:55 +0700

    Add banner and text component.

--- feature/adaptivecards/src/main/java/com/adedom/adaptivecards/presentation/component/AdaptiveCardScreen.kt
+++ feature/adaptivecards/src/main/java/com/adedom/adaptivecards/presentation/component/AdaptiveCardScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import com.adedom.adaptivecards.data.models.Component
 import com.adedom.adaptivecards.presentation.event.AdaptiveCardUiEvent
 import com.adedom.adaptivecards.presentation.viewmodel.AdaptiveCardViewModel
 
@@ -17,9 +18,41 @@ fun AdaptiveCardScreen(
         viewModel.onEvent(AdaptiveCardUiEvent.Initial)
     }
 
+    AdaptiveCardContent(uiState.components)
+}
+
+@Composable
+fun AdaptiveCardContent(components: List<Component>) {
     LazyColumn {
-        items(uiState.components) { component ->
-            ComponentText(component = component)
+        items(components) { component ->
+            UiComponentRender(component)
         }
+    }
+}
+
+@Composable
+fun UiComponentRender(component: Component) {
+    when (component) {
+//        is Component.ActionOpenUrl -> ActionOpenUrlText(component = component)
+//        is Component.ActionShowCard -> ActionShowCardText(component = component)
+//        is Component.ActionSubmit -> ActionSubmitText(component = component)
+//        is Component.AdaptiveCard -> AdaptiveCardText(component = component)
+//        is Component.Column -> ColumnText(component = component)
+//        is Component.ColumnSet -> ColumnSetText(component = component)
+//        is Component.FactSet -> FactSetText(component = component)
+//        is Component.Image -> ImageText(component = component)
+//        is Component.InputDate -> InputDateText(component = component)
+//        is Component.InputText -> InputTextText(component = component)
+//        is Component.TextBlock -> TextBlockText(component = component)
+        is Component.Banner -> BannerComponent(component = component)
+        is Component.Text -> TextComponent(component = component)
+//        is Component.Container -> ContainerText(component = component)
+//        is Component.Cards -> CardsText(component = component)
+//        is Component.Button -> ButtonText(component = component)
+//        is Component.Badge -> BadgeText(component = component)
+//        is Component.ActionOpenMore -> ActionOpenMoreText(component = component)
+//        is Component.TextBadge -> TextBadgeText(component = component)
+//        is Component.LazyHorizontal -> LazyHorizontalText(component = component)
+        else -> {}
     }
 }
---
+++ feature/adaptivecards/src/main/java/com/adedom/adaptivecards/presentation/component/BannerComponent.kt
@@ -0,0 +1,59 @@
+package com.adedom.adaptivecards.presentation.component
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.adedom.adaptivecards.data.models.Component
+import com.adedom.ui_component.ui.theme.Typography
+
+@Composable
+fun BannerComponent(
+    modifier: Modifier = Modifier,
+    component: Component.Banner
+) {
+    Box(modifier = modifier) {
+        AsyncImage(
+            model = component.url,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .height(150.dp)
+                .fillMaxSize()
+                .padding(16.dp)
+                .clip(RoundedCornerShape(16.dp))
+        )
+
+        Column(
+            modifier = Modifier
+                .align(Alignment.BottomStart)
+                .padding(32.dp)
+        ) {
+            component.info?.title?.let {
+                Text(
+                    text = it,
+                    style = Typography.h5,
+                    color = MaterialTheme.colors.primary
+                )
+            }
+            component.info?.description?.let {
+                Text(
+                    text = it,
+                    style = Typography.body2,
+                    color = MaterialTheme.colors.primary
+                )
+            }
+        }
+    }
+}
