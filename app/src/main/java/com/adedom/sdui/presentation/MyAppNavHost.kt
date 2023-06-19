d849cb23725b2bf2902dadc2ba8694f5047b2b70
pathiphon <pathiphon@ch7.com>
2023-06-19 15:28:12 +0700

    setup deeplink.

--- app/src/main/java/com/adedom/sdui/presentation/MyAppNavHost.kt
+++ app/src/main/java/com/adedom/sdui/presentation/MyAppNavHost.kt
@@ -1,11 +1,17 @@
 package com.adedom.sdui.presentation
 
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.Intent
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.net.toUri
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navDeepLink
 import com.adedom.adaptivecards.data.models.Action
 import com.adedom.adaptivecards.presentation.component.AdaptiveCardScreen
 import org.koin.androidx.compose.koinViewModel
@@ -21,31 +27,11 @@ fun MyAppNavHost(
         navController = navController,
         startDestination = startDestination
     ) {
-        composable("main") {
-            AdaptiveCardScreen(
-                viewModel = koinViewModel(),
-                onClick = { action ->
-                    when (action) {
-                        is Action.OpenMore -> {
-                            navController.navigate("details/${action.id}")
-                        }
-
-                        is Action.OpenUrl -> {
-                            navController.navigate("details/${action.id}")
-                        }
-
-                        is Action.ShowCard -> {
-                            navController.navigate("details/${action.id}")
-                        }
-
-                        is Action.Submit -> {
-                            navController.navigate("details/${action.id}")
-                        }
-                    }
-                }
-            )
-        }
-        composable("details/{id}") { backStackEntry ->
+        composable(
+            route = "main",
+            deepLinks = listOf(navDeepLink { uriPattern = "sdui://adaptivecards/{id}" })
+        ) { backStackEntry ->
+            val context = LocalContext.current
             val id = backStackEntry.arguments?.getInt("id")
 
             AdaptiveCardScreen(
@@ -54,23 +40,37 @@ fun MyAppNavHost(
                 onClick = { action ->
                     when (action) {
                         is Action.OpenMore -> {
-                            navController.navigate("details/${action.id}")
+                            context.openDeeplink(action.id)
                         }
 
                         is Action.OpenUrl -> {
-                            navController.navigate("details/${action.id}")
+                            context.openDeeplink(action.id)
                         }
 
                         is Action.ShowCard -> {
-                            navController.navigate("details/${action.id}")
+                            context.openDeeplink(action.id)
                         }
 
                         is Action.Submit -> {
-                            navController.navigate("details/${action.id}")
+                            context.openDeeplink(action.id)
                         }
                     }
                 }
             )
         }
+    }
+}
+
+private fun Context.openDeeplink(id: Int?) {
+    try {
+        val deepLinkIntent = Intent(
+            Intent.ACTION_VIEW,
+            "sdui://adaptivecards/${id}".toUri(),
+            this,
+            MainActivity::class.java
+        )
+        this.startActivity(deepLinkIntent)
+    } catch (e: ActivityNotFoundException) {
+        e.printStackTrace()
     }
 }
